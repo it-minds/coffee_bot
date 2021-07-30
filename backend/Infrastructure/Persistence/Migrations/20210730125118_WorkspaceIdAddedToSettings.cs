d22f00e42c514b3e3b096957278d08a97b334c2d
Martin Johannesson <mjh@it-minds.dk>
2021-07-30 14:51:53 +0200

    Added missing migration

---
+++ backend/Infrastructure/Persistence/Migrations/20210730125118_WorkspaceIdAddedToSettings.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class WorkspaceIdAddedToSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SlackChannelId",
+                table: "CoffeeRounds");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SlackWorkSpaceId",
+                table: "ChannelSettings",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SlackWorkSpaceId",
+                table: "ChannelSettings");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SlackChannelId",
+                table: "CoffeeRounds",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
