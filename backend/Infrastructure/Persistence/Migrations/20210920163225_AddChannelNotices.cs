a1427ece7895e64ff6436ea6cd6d8d4b3e5ea11d
Martin Johannesson <mjh@it-minds.dk>
2021-09-20 18:33:40 +0200

    Migration added

---
+++ backend/Infrastructure/Persistence/Migrations/20210920163225_AddChannelNotices.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddChannelNotices : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChannelNotices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoticeType = table.Column<int>(type: "int", nullable: false),
+                    DaysInRound = table.Column<int>(type: "int", nullable: false),
+                    Enabled = table.Column<bool>(type: "bit", nullable: false),
+                    Personal = table.Column<bool>(type: "bit", nullable: false),
+                    ChannelSettingsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChannelNotices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChannelNotices_ChannelSettings_ChannelSettingsId",
+                        column: x => x.ChannelSettingsId,
+                        principalTable: "ChannelSettings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChannelNotices_ChannelSettingsId",
+                table: "ChannelNotices",
+                column: "ChannelSettingsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChannelNotices");
+        }
+    }
+}
