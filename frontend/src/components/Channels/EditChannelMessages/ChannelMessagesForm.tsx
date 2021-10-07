f62374657d61e649f829bdb70eb3f3edd6b79314
Michael Jeppesen <mjp@it-minds.dk>
2021-10-07 11:35:35 +0200

    Added required tags for each of the messages in frontend

--- frontend/src/components/Channels/EditChannelMessages/ChannelMessagesForm.tsx
+++ frontend/src/components/Channels/EditChannelMessages/ChannelMessagesForm.tsx
@@ -1,17 +1,16 @@
 import {
   Box,
+  Text,
   Button,
   Flex,
   FormControl,
   FormLabel,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
+  UnorderedList,
+  ListItem,
   Textarea,
-  Switch
+  FormErrorMessage
 } from "@chakra-ui/react";
+import { Console } from "console";
 import { useEffectAsync } from "hooks/useEffectAsync";
 import { useNSwagClient } from "hooks/useNSwagClient";
 import React, { FC, useCallback, useState } from "react";
@@ -51,7 +50,8 @@ const ChannelMessagesForm: FC<Props> = ({ submitCallback, channelId }) => {
     async event => {
       event.preventDefault();
       setIsLoading(true);
-      await submitCallback(localFormData);
+      if (validateFields())
+        await submitCallback(localFormData);
       setIsLoading(false);
     },
     [localFormData, submitCallback]
@@ -64,35 +64,74 @@ const ChannelMessagesForm: FC<Props> = ({ submitCallback, channelId }) => {
     });
   }, []);
 
+  const [ validFields, setValidFields ] = useState([true, true, true, true]);
+  const validateFields = useCallback(() => {
+    var valid = [true, true, true, true];
+
+    valid[0] &&= /{{\s*[rR]ound[sS]tart[tT]ime\s*}}/g.test(localFormData["roundStartChannelMessage"]);
+    valid[0] &&= /{{\s*[rR]ound[eE]nd[tT]ime\s*}}/g.test(localFormData["roundStartChannelMessage"]);
+    valid[0] &&= /{{\s*[gG]roups\s*}}/g.test(localFormData["roundStartChannelMessage"]);
+
+    valid[1] &&= /{{\s*[rR]ound[sS]tart[tT]ime\s*}}/.test(localFormData["roundStartGroupMessage"]);
+    valid[1] &&= /{{\s*[rR]ound[eE]nd[tT]ime\s*}}/.test(localFormData["roundStartGroupMessage"]);
+
+    valid[2] &&= /{{\s*[yY]es[bB]utton\s*}}/.test(localFormData["roundMidwayMessage"]);
+    valid[2] &&= /{{\s*[nN]o[bB]utton\s*}}/.test(localFormData["roundMidwayMessage"]);
+
+    setValidFields(valid);
+    console.log(valid);
+    return valid[0] && valid[1] && valid[2] && valid[3];
+  }, [localFormData])
+
   return (
     <Flex w="full" align="center" justifyContent="center">
       <Box width="md" opacity={isLoading ? 0.2 : 1}>
+        <Text>
+          This is for changing the messages the coffee-bot will send to this channel. <br />
+          The following tags can be used as variables in the messages:
+          <UnorderedList my={5}>
+            <ListItem>&#123;&#123; MeetupPercentage &#125;&#125;: Will print the number representing the percent of people who have met up for coffee so far this round</ListItem>
+            <ListItem>&#123;&#123; MeetupCondition &#125;&#125;: Will print the line "Next time, let's try for 100% shall we?" if MeetupPercentage is not 100%</ListItem>
+            <ListItem>&#123;&#123; RoundStartTime &#125;&#125;: Will print the start date for the round</ListItem>
+            <ListItem>&#123;&#123; RoundEndTime &#125;&#125;: Will print the end date for the round</ListItem>
+            <ListItem>&#123;&#123; Groups &#125;&#125;: Wil print the list of all groups</ListItem>
+          </UnorderedList>
+          And the following two only work in the "Midway Message"
+          <UnorderedList>
+            <ListItem>&#123;&#123; YesButton &#125;&#125;: Specifies where in the message the yes button will be</ListItem>
+            <ListItem>&#123;&#123; NoButton &#125;&#125;: Specifies where in the message the no button will be</ListItem>
+          </UnorderedList>
+          In order to ping a group use "&#60;!group to ping&#62;"
+        </Text>
         <form onSubmit={onSubmit}>
-          <FormControl isRequired>
+          <FormControl mt={5} isRequired isInvalid={!validFields[0]}>
             <FormLabel>Round Start Channel Message</FormLabel>
             <Textarea
               placeholder="Round Start Channel Message"
               value={localFormData.roundStartChannelMessage}
               onChange={event => updateLocalForm(String(event.target.value), "roundStartChannelMessage")}
               />
+              <FormErrorMessage>This message must contain the following tags: &#123;&#123; RoundStartTime &#125;&#125;, &#123;&#123; RoundEndTime &#125;&#125; and &#123;&#123; Groups &#125;&#125;</FormErrorMessage>
           </FormControl>
-          <FormControl isRequired>
+          <FormControl isRequired isInvalid={!validFields[1]}>
             <FormLabel>Round Start Group Message</FormLabel>
             <Textarea
               placeholder="Round Start Group Message"
               value={localFormData.roundStartGroupMessage}
               onChange={event => updateLocalForm(String(event.target.value), "roundStartGroupMessage")}
               />
+              <FormErrorMessage>This message must contain the following tags: &#123;&#123; RoundStartTime &#125;&#125; and &#123;&#123; RoundEndTime &#125;&#125;</FormErrorMessage>
           </FormControl>
-          <FormControl isRequired>
+          <FormControl isRequired isInvalid={!validFields[2]}>
             <FormLabel>Round Midway Message</FormLabel>
             <Textarea
               placeholder="Round Midway Message"
               value={localFormData.roundMidwayMessage}
               onChange={event => updateLocalForm(String(event.target.value), "roundMidwayMessage")}
               />
+              <FormErrorMessage>This message must contain the following tags: &#123;&#123; YesButton &#125;&#125; and &#123;&#123; NoButton &#125;&#125;</FormErrorMessage>
           </FormControl>
-          <FormControl isRequired>
+          <FormControl isRequired isInvalid={!validFields[3]}>
             <FormLabel>Round Finisher Message</FormLabel>
             <Textarea
               placeholder="Round Finisher Message"
