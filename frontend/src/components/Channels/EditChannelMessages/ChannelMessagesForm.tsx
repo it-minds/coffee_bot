3a3bb9776ec4aab942e9748665819d94bac84cdf
Michael Jeppesen <mjp@it-minds.dk>
2021-10-11 12:58:45 +0200

    MessageForm now uses defaults and predicates from backend

--- frontend/src/components/Channels/EditChannelMessages/ChannelMessagesForm.tsx
+++ frontend/src/components/Channels/EditChannelMessages/ChannelMessagesForm.tsx
@@ -31,6 +31,8 @@ const defaultChannel: ChannelSettingsDto = {
 const ChannelMessagesForm: FC<Props> = ({ submitCallback, channelId }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [localFormData, setLocalFormData] = useState<ChannelSettingsDto>(defaultChannel);
+  const [requiredTags, setRequiredTags] = useState<string[][]>(null);
+  const [predicates, setPredicates] = useState<{ [key: string]: string }>(null);
 
   const { genClient } = useNSwagClient(ChannelClient);
 
@@ -39,18 +41,32 @@ const ChannelMessagesForm: FC<Props> = ({ submitCallback, channelId }) => {
       setIsLoading(true);
       const client = await genClient();
       const channel = await client.getChannelSettings(channelId);
-
       setLocalFormData(channel);
       setIsLoading(false);
     }
   }, []);
 
-  const onSubmit = useCallback(async event => {
-    event.preventDefault();
-    setIsLoading(true);
-    if (validateFields()) await submitCallback(localFormData);
-    setIsLoading(false);
+  useEffectAsync(async () => {
+    const client = await genClient();
+    const tagData = await client.getRequiredTags();
+    setPredicates(tagData.tagToPredicate);
+    setRequiredTags([
+      tagData.startChannelMessageRequiredTags,
+      tagData.startGroupMessageRequiredTags,
+      tagData.midwayMessageRequiredTags,
+      tagData.finisherMessageRequiredTags
+    ]);
   }, []);
+
+  const onSubmit = useCallback(
+    async event => {
+      event.preventDefault();
+      setIsLoading(true);
+      if (validateFields()) await submitCallback(localFormData);
+      setIsLoading(false);
+    },
+    [localFormData, requiredTags, predicates]
+  );
   const updateLocalForm = useCallback((value: unknown, key: keyof ChannelSettingsDto) => {
     setLocalFormData(form => {
       (form[key] as unknown) = value;
@@ -62,22 +78,22 @@ const ChannelMessagesForm: FC<Props> = ({ submitCallback, channelId }) => {
   const validateFields = useCallback(() => {
     const valid = [true, true, true, true];
 
-    valid[0] &&= /{{\s*[rR]ound[sS]tart[tT]ime\s*}}/g.test(
-      localFormData["roundStartChannelMessage"]
-    );
-    valid[0] &&= /{{\s*[rR]ound[eE]nd[tT]ime\s*}}/g.test(localFormData["roundStartChannelMessage"]);
-    valid[0] &&= /{{\s*[gG]roups\s*}}/g.test(localFormData["roundStartChannelMessage"]);
-
-    valid[1] &&= /{{\s*[rR]ound[sS]tart[tT]ime\s*}}/.test(localFormData["roundStartGroupMessage"]);
-    valid[1] &&= /{{\s*[rR]ound[eE]nd[tT]ime\s*}}/.test(localFormData["roundStartGroupMessage"]);
-
-    valid[2] &&= /{{\s*[yY]es[bB]utton\s*}}/.test(localFormData["roundMidwayMessage"]);
-    valid[2] &&= /{{\s*[nN]o[bB]utton\s*}}/.test(localFormData["roundMidwayMessage"]);
+    requiredTags[0].forEach(tag => {
+      valid[0] &&= RegExp(predicates[tag]).test(localFormData["roundStartChannelMessage"]);
+    });
+    requiredTags[1].forEach(tag => {
+      valid[1] &&= RegExp(predicates[tag]).test(localFormData["roundStartGroupMessage"]);
+    });
+    requiredTags[2].forEach(tag => {
+      valid[2] &&= RegExp(predicates[tag]).test(localFormData["roundMidwayMessage"]);
+    });
+    requiredTags[3].forEach(tag => {
+      valid[3] &&= RegExp(predicates[tag]).test(localFormData["roundFinisherMessage"]);
+    });
 
     setValidFields(valid);
-    console.log(valid);
     return valid[0] && valid[1] && valid[2] && valid[3];
-  }, [localFormData]);
+  }, [localFormData, predicates, requiredTags]);
 
   return (
     <Flex w="full" align="center" justifyContent="center">
@@ -85,36 +101,24 @@ const ChannelMessagesForm: FC<Props> = ({ submitCallback, channelId }) => {
         <Text>
           This is for changing the messages the coffee-bot will send to this channel. <br />
           The following tags can be used as variables in the messages:
-          <UnorderedList my={5}>
-            <ListItem>
-              &#123;&#123; MeetupPercentage &#125;&#125;: Will print the number representing the
-              percent of people who have met up for coffee so far this round
-            </ListItem>
-            <ListItem>
-              &#123;&#123; MeetupCondition &#125;&#125;: Will print the line &quot;Next time,
-              let&apos;s try for 100% shall we?&quot; if MeetupPercentage is not 100%
-            </ListItem>
-            <ListItem>
-              &#123;&#123; RoundStartTime &#125;&#125;: Will print the start date for the round
-            </ListItem>
-            <ListItem>
-              &#123;&#123; RoundEndTime &#125;&#125;: Will print the end date for the round
-            </ListItem>
-            <ListItem>&#123;&#123; Groups &#125;&#125;: Wil print the list of all groups</ListItem>
-          </UnorderedList>
-          And the following two only work in the &quot;Midway Message&quot;
-          <UnorderedList>
-            <ListItem>
-              &#123;&#123; YesButton &#125;&#125;: Specifies where in the message the yes button
-              will be
-            </ListItem>
-            <ListItem>
-              &#123;&#123; NoButton &#125;&#125;: Specifies where in the message the no button will
-              be
-            </ListItem>
-          </UnorderedList>
-          In order to ping a group use &quot;&#60;!group to ping&#62;&quot;
         </Text>
+        <UnorderedList my={5}>
+          {predicates != null &&
+            Object.keys(predicates).map(key => {
+              return <ListItem key={key}>{key}</ListItem>;
+            })}
+        </UnorderedList>
+        The following two only work in the &quot;Midway Message&quot;
+        <UnorderedList>
+          <ListItem>
+            &#123;&#123; YesButton &#125;&#125;: Specifies where in the message the yes button will
+            be
+          </ListItem>
+          <ListItem>
+            &#123;&#123; NoButton &#125;&#125;: Specifies where in the message the no button will be
+          </ListItem>
+        </UnorderedList>
+        <Text>In order to ping a group use &quot;&#60;!group to ping&#62;&quot;</Text>
         <form onSubmit={onSubmit}>
           <FormControl mt={5} isRequired isInvalid={!validFields[0]}>
             <FormLabel>Round Start Channel Message</FormLabel>
@@ -126,9 +130,7 @@ const ChannelMessagesForm: FC<Props> = ({ submitCallback, channelId }) => {
               }
             />
             <FormErrorMessage>
-              This message must contain the following tags: &#123;&#123; RoundStartTime
-              &#125;&#125;, &#123;&#123; RoundEndTime &#125;&#125; and &#123;&#123; Groups
-              &#125;&#125;
+              This message must contain the following tags: {requiredTags[0].join(", ")}
             </FormErrorMessage>
           </FormControl>
           <FormControl isRequired isInvalid={!validFields[1]}>
@@ -141,8 +143,7 @@ const ChannelMessagesForm: FC<Props> = ({ submitCallback, channelId }) => {
               }
             />
             <FormErrorMessage>
-              This message must contain the following tags: &#123;&#123; RoundStartTime &#125;&#125;
-              and &#123;&#123; RoundEndTime &#125;&#125;
+              This message must contain the following tags: {requiredTags[1].join(", ")}
             </FormErrorMessage>
           </FormControl>
           <FormControl isRequired isInvalid={!validFields[2]}>
@@ -153,8 +154,7 @@ const ChannelMessagesForm: FC<Props> = ({ submitCallback, channelId }) => {
               onChange={event => updateLocalForm(String(event.target.value), "roundMidwayMessage")}
             />
             <FormErrorMessage>
-              This message must contain the following tags: &#123;&#123; YesButton &#125;&#125; and
-              &#123;&#123; NoButton &#125;&#125;
+              This message must contain the following tags: {requiredTags[2].join(", ")}
             </FormErrorMessage>
           </FormControl>
           <FormControl isRequired isInvalid={!validFields[3]}>
@@ -166,6 +166,9 @@ const ChannelMessagesForm: FC<Props> = ({ submitCallback, channelId }) => {
                 updateLocalForm(String(event.target.value), "roundFinisherMessage")
               }
             />
+            <FormErrorMessage>
+              This message must contain the following tags: {requiredTags[3].join(", ")}
+            </FormErrorMessage>
           </FormControl>
           <Button colorScheme="green" isLoading={isLoading} mt={6} type="submit">
             Submit
