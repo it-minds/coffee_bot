74e39eaebb03978f793fc75dfea4d92b0dc635ea
Martin Johannesson <mjh@it-minds.dk>
2021-11-30 16:27:56 +0100

    Color of the barcharts

--- frontend/src/pages/channels/[channelId]/rounds/index.tsx
+++ frontend/src/pages/channels/[channelId]/rounds/index.tsx
@@ -1,45 +1,49 @@
-import { Button, HStack, Input, Text } from "@chakra-ui/react";
+import { HStack, Input, Text, useColorModeValue, useToken } from "@chakra-ui/react";
 import { ChartData, ChartOptions } from "chart.js";
-import { AuthContext } from "contexts/AuthContext";
 import { withAuth } from "hocs/withAuth";
 import { useEffectAsync } from "hooks/useEffectAsync";
 import { useNSwagClient } from "hooks/useNSwagClient";
 import { NextPage } from "next";
 import { useRouter } from "next/dist/client/router";
-import React, { useCallback, useContext, useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Bar } from "react-chartjs-2";
 import { ChannelClient, RoundSnipDto } from "services/backend/nswagts";
 import { dateTimeFormatter } from "utils/formatters/dateTimeFormatter";
 
 const IndexPage: NextPage = () => {
-  const { activeUser } = useContext(AuthContext);
   const router = useRouter();
+  const channelId = useMemo(() => parseInt(router.query.channelId as string), [router.query]);
 
-  const [channelId, setChannelId] = useState(0);
   const [fromDate, setFromDate] = useState<string>(
-    // 5 weeks ago as default value
-    new Date(new Date().valueOf() - 5 * 7 * 24 * 3600 * 1000).toISOString().substring(0, 10)
+    () =>
+      (router.query.from as string) ??
+      new Date(new Date().valueOf() - 5 * 7 * 24 * 3600 * 1000).toISOString().substring(0, 10)
   );
   const [toDate, setToDate] = useState<string>(new Date().toISOString().substring(0, 10));
   const [rounds, setRounds] = useState<RoundSnipDto[]>([]);
 
   const { genClient } = useNSwagClient(ChannelClient);
 
-  const fetchRounds = useCallback(async () => {
+  useEffectAsync(async () => {
     const client = await genClient();
     const result = await client.getRoundsInRange(channelId, new Date(fromDate), new Date(toDate));
 
     setRounds(result);
-  }, [fromDate, toDate, setRounds, channelId]);
-
-  useEffectAsync(async () => {
-    if (!activeUser || !router.query.channelId) return;
-    setChannelId(parseInt(router.query.channelId as string));
-  }, [activeUser, router.query]);
+  }, [fromDate, toDate, channelId]);
 
   useEffect(() => {
-    fetchRounds();
-  }, [channelId]);
+    const query = `?from=${fromDate}&to=${toDate}`;
+    const safeAsPath = router.asPath.split("?")[0];
+
+    router.replace(router.pathname + query, safeAsPath + query, {
+      shallow: true
+    });
+  }, [fromDate, toDate, router.pathname]);
+
+  const [bar1Color, bar2Color] = useToken("colors", [
+    useColorModeValue("blue.300", "blue.700"),
+    useColorModeValue("pink.400", "pink.700")
+  ]);
 
   const [data, setData] = useState<ChartData>();
   const [options, setOptions] = useState<ChartOptions>();
@@ -55,12 +59,12 @@ const IndexPage: NextPage = () => {
         {
           label: "Meetup percentage",
           data: rounds.map(round => (round.meetupPercentage > 0 ? round.meetupPercentage : 1)),
-          backgroundColor: "rgb(255, 0, 0)"
+          backgroundColor: bar1Color
         },
         {
           label: "Photo percentage",
           data: rounds.map(round => (round.photoPercentage > 0 ? round.photoPercentage : 1)),
-          backgroundColor: "rgb(0, 255, 0)"
+          backgroundColor: bar2Color
         }
       ]
     });
@@ -83,35 +87,26 @@ const IndexPage: NextPage = () => {
         }
       }
     });
-  }, [rounds]);
+  }, [rounds, bar1Color, bar2Color]);
 
   return (
     <>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          fetchRounds();
-        }}>
-        <HStack>
-          <Text>Date interval: From: </Text>
-          <Input
-            type="date"
-            w="xsm"
-            value={fromDate}
-            onChange={event => setFromDate(event.target.value)}
-          />
-          <Text> To: </Text>
-          <Input
-            type="date"
-            w="xsm"
-            value={toDate}
-            onChange={event => setToDate(event.target.value)}
-          />
-          <Input type="submit" as={Button} w="sm">
-            Get rounds from selected interval
-          </Input>
-        </HStack>
-      </form>
+      <HStack>
+        <Text>Date interval: From: </Text>
+        <Input
+          type="date"
+          w="xsm"
+          value={fromDate}
+          onChange={event => setFromDate(event.target.value)}
+        />
+        <Text> To: </Text>
+        <Input
+          type="date"
+          w="xsm"
+          value={toDate}
+          onChange={event => setToDate(event.target.value)}
+        />
+      </HStack>
       <Bar
         data={data}
         options={options}
